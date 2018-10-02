cd7e911501966273fa376301d43bea1a91415bc9
Adam Morsi <adam.morsi@yahoo.com>
2018-10-02 17:01:05 +0300

    Assignment submission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms-td-assignment';
+    subscriptionTypes = ['Basic', 'Advanced', 'Pro'];
+    defaultSubscriptionType = 'Advanced';
+    data = {
+        email: '',
+        sType: '',
+        password: ''
+    };
+
+    onSubmit(form: NgForm) {
+        this.data.email = form.value.email;
+        this.data.sType = form.value.subscription;
+        this.data.password = form.value.password;
+
+        form.reset();
+    }
 }
